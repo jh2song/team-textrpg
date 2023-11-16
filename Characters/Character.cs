e0575eb532779e2c574a9fb2723143224ca990d9
Noname136-rpg <zofhfh2323@gmail.com>
2023-11-16 15:56:06 +0900

    kim ho young
    1. dungeon 폴더로  dungeonManager.cs 이동
    2. Character.cs의 TakeDamage 수정

--- Characters/Character.cs
+++ Characters/Character.cs
@@ -14,16 +14,39 @@ namespace text_rpg.Characters
         public int Defence { get; set; }
         public int Hp { get; set; }
         public int Gold { get; set; }
-        public bool IsDead { get; set; }
+        public bool IsDead = false;
         /// <summary>
         /// 크리티컬 확률
         /// </summary>
-        public float CritRate { get; set; }
-        public float MissRate { get; set; }
+        public int CritRate { get; set; }
+        public int MissRate { get; set; }
 
-        public void TakeDamage(int damage) // 매개변수 임의로 작성
+        public void TakeDamage(Character enemy) // 매개변수 임의로 작성
         {
-            // TODO
+            // 데미지 오차 랜덤
+            int damageRange = (int)Math.Ceiling((double)(enemy.Attack / 10));
+            int Damage = rand.Next(enemy.Attack - damageRange, enemy.Attack + damageRange);
+
+            //1.치명타인지 아닌지 확인
+            if (rand.Next(1, 101) > CritRate)
+            {
+                Damage = Damage * 2;
+            }
+            else
+            {
+                //2. 회피하였는지 아닌지 확인
+                if (rand.Next(1, 101) > MissRate)
+                {
+                    Damage = (int)Math.Ceiling((double)(Damage / 2));
+                }
+            }
+
+            //받는 데미지에서 방어력 만큼 뺀 수치만큼 HP를 깎는다.
+            Hp -= (Damage - Defence);
+            if (Hp <= 0)
+            {
+                IsDead = true;
+            }
         }
 
         public void Dead()
