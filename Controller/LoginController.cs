0dfb76d3c7c542c7037f7776013d34c1d2491754
levell1 <whddnr0317@naver.com>
2023-11-17 12:15:57 +0900

    [ADD] 로그인 C,V 추가 V 추가작업 필요

--- Controller/LoginController.cs
+++ Controller/LoginController.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using text_rpg.Utils;
 using text_rpg.View;
-
+using text_rpg.Characters;
 namespace text_rpg.Controller
 {
     internal class LoginController
@@ -28,8 +28,23 @@ namespace text_rpg.Controller
 
         public void Login()
         {
+            Console.WriteLine("아이디(ID)를 입력해 주세요");
+            String _InputId = Console.ReadLine();
+            if (_id != null && _InputId == _id)
+            {
+                _InputId = _id;
+                Console.WriteLine("비밀번호(PassWord)를 입력해 주세요");
+                String _InputpassWord = Console.ReadLine();
+                if (_InputpassWord != null && _password == _InputpassWord)
+                {
+                    MainController.getInst().CurrentStage = Define.GameStages.Base;
+                }
+            }
+            else {
+                Login();
+            }
 
-            MainController.getInst().CurrentStage = Define.GameStages.Base;
+            _loginView.View(_id);
         }
     }
 }
--- View/LoginView.cs
+++ View/LoginView.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using text_rpg.Controller;
+using text_rpg.Characters;
 namespace text_rpg.View
 {
     internal class LoginView
     {
-        public void View()
+        public void View(string id)
         {
+            Console.WriteLine($"{id}님 로그인에 성공 하셨습니다.");
+            // 추가 View 
 
         }
     }
