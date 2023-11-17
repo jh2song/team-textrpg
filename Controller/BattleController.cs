5286600337b91305aed20a7c495248482aa469e4
levell1 <whddnr0317@naver.com>
2023-11-17 11:13:02 +0900

    [Fix] BattleContriller 재배치-> view,Controller

--- Controller/BattleController.cs
+++ Controller/BattleController.cs
@@ -10,12 +10,129 @@ namespace text_rpg.Controller
 {
     internal class BattleController
     {
-        public List<Monster> Monsters;
+
         private BattleView _battleView;
 
-        public void Battle()
+        //Player player = new Player("버스타조",1,10,5,100,1500,5,5);
+        public List<Monster> CreateMonsters { get; set; }
+
+
+        public void StartDungeon(int stage)
+        {
+
+            int monsternum = 0;
+            if (stage == 1) { monsternum = 4; }            //난이도
+            else if (stage == 2) { monsternum = 7; }
+
+            CreateMonsters = new List<Monster>();
+            CreateMonsters.Clear();
+            Random rand = new Random();
+            int MonsterNumber = rand.Next(3, 4);    // 2마리~4마리
+            int MonsterType;
+
+            for (int i = 0; i < MonsterNumber; i++)
+            {
+
+                MonsterType = rand.Next(monsternum, monsternum + 3);   //몬스터 데이터 보고 조정   
+                Monster monsterinfo = monsterData.ElementAt(MonsterType).Value;
+                CreateMonsters.Add(monsterinfo);
+                Console.WriteLine($"LV.{monsterinfo.Level} \t {monsterinfo.Name} \t HP : {monsterinfo.Hp} \t ATK : {monsterinfo.Attack},");
+            }
+
+        }
+        public void InDungeon(Monster monster)
+        {
+            //전투 씬
+            //enum switch -> 스킬쓸지 공격할지 방어?회피? 선택지
+        }
+        public void DungeonClear(Monster monster)
+        {
+
+            CreateMonsters.Clear();
+        }
+
+        public Dictionary<int, Monster> monsterData;
+
+        public string path = "MonsterData.csv";
+        public void LoadMosters()
+        {
+            monsterData = new Dictionary<int, Monster>();
+            monsterData.Clear();
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(new FileStream(path, FileMode.Open)))
+                {
+                    sr.ReadLine();
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+
+                        string[] data = line.Split(',');
+
+                        Monster monster = new Monster(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]);
+                        monsterData.Add(monster.Id, monster);
+
+                    }
+                }
+            }
+        }
+
+        private void battleView()
+        {
+            //던전 입구 출력
+            /*do
+            {
+                switch (d_phase)
+                {
+                    case dungeonSelect:
+                        break;
+                    case UseItem:
+                        break;
+                    case battle:
+                            battle 내부
+                             while(Player.isDead!=true || CreateMonster.Length != 0) 
+                        break;
+                    case getOut;
+                        break;
+                }
+            }while
+            
+            battel 내부
+            뷰
+            for(int cnt = 0; cnt < CreateMonster.Length; cnt++)
+            {
+                if(CreateMonster[cnt].IsDead == true)
+                { 
+                    출력은 하는데  LV.3 공허충 Dead 로 나오도록
+                }
+            }
+
+            전투
+            for(int cnt = 0; cnt < CreateMonster.Length; cnt++)
+            {
+                if(CreateMonster[cnt].IsDead != true)
+                { 
+                    전투
+                }
+            }
+            
+            }
+             */
+
+            //선택
+
+            //던전 내
+        }
+
+        //외부로 화면 출력코드를 빼기
+
+        void BattleMonster(int level)
         {
 
         }
+        public void Battle() { }
+
+
     }
 }
--- View/BattleView.cs
+++ View/BattleView.cs
@@ -5,14 +5,95 @@ using System.Text;
 using System.Threading.Tasks;
 using text_rpg.Characters;
 using text_rpg.Controller;
+using text_rpg.dungeon;
 
 namespace text_rpg.View
 {
+    enum Select
+    {
+        exit = 0,
+        EnterDungeon,
+        UseItem = 2
+    }
+
     internal class BattleView
     {
         public void View(List<Monster> monsters)
         {
             
+        }
+
+        public void DungeonSelectView()
+        {
+            Console.WriteLine("==================================================");
+            Console.WriteLine("  ┏   ┓             ◆ ;");
+            Console.WriteLine(" |      |          └┼┐ == ");
+            Console.WriteLine("|        |         ┌│  ==");
+            Console.WriteLine("==================================================");
+            Console.WriteLine("======== 던전에 가기 전 준비를 해주세요. =========\n\n");
+            Console.WriteLine("==================");
+            Console.WriteLine("= 1. 던전 입장   =");
+            Console.WriteLine("= 2. 소모품 사용 =");
+            Console.WriteLine("= 0. 나가기      =");
+            Console.WriteLine("==================");
+        }
+        private void dungeonEnter()
+        {
+            //종욱님 그림
+            DungeonSelectView();
+
+            int firstView = (int)Select.EnterDungeon;
+            firstView = CheckValidInput(0, 2);
+            Select enumValue = (Select)firstView;
+
+            switch (enumValue)
+            {
+                case Select.EnterDungeon:
+                    // phase = d_phase.battle
+                    // 던전입장
+                    break;
+
+                case Select.UseItem:
+
+                    break;
+
+                case Select.exit:
+                    // 메인으로 나가기
+                    break;
+
+            }
+        }
+        public void WinBoard(List<Monster> monster, Player player)
+        {
+            int rewardGold;
+            int rewardExp;
+            rewardGold = monster[0].Gold + monster[1].Gold + monster[2].Gold;
+            rewardExp = monster[0].RewardExp + monster[1].RewardExp + monster[2].RewardExp;
+            player.Gold += rewardGold;
+            player.LevelUpExp += rewardExp;
+            // stage++;
+        }
+
+        public void LoseBoard()
+        {
+            Console.WriteLine("키 입력으로  던전 입구로 돌아가기");
+        }
+
+        static int CheckValidInput(int min, int max)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                bool parseSuccess = int.TryParse(input, out var ret);
+                if (parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
+
+                Console.WriteLine("잘못된 입력입니다.");
+            }
         }
     }
 }
