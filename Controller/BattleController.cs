857e18c5f415e93d1b79689ef73b27e8c7c1b32d
levell1 <whddnr0317@naver.com>
2023-11-17 11:33:18 +0900

    [Fix] BattlerController 메서드 재배치-> view,Controller

--- Controller/BattleController.cs
+++ Controller/BattleController.cs
@@ -11,8 +11,8 @@ namespace text_rpg.Controller
     internal class BattleController
     {
 
-        private BattleView _battleView;
-
+        private BattleView _battleView = new BattleView();
+        
         //Player player = new Player("버스타조",1,10,5,100,1500,5,5);
         public List<Monster> CreateMonsters { get; set; }
 
@@ -49,6 +49,33 @@ namespace text_rpg.Controller
         {
 
             CreateMonsters.Clear();
+        }
+
+        public void dungeonEnter()
+        {
+            //종욱님 그림
+            _battleView.DungeonSelectView();
+
+            int firstView = (int)Select.EnterDungeon;
+            firstView = CheckValidInput(0, 2);
+            Select enumValue = (Select)firstView;
+
+            switch (enumValue)
+            {
+                case Select.EnterDungeon:
+                    // phase = d_phase.battle
+                    // 던전입장
+                    break;
+
+                case Select.UseItem:
+
+                    break;
+
+                case Select.exit:
+                    // 메인으로 나가기
+                    break;
+
+            }
         }
 
         public Dictionary<int, Monster> monsterData;
@@ -133,6 +160,32 @@ namespace text_rpg.Controller
         }
         public void Battle() { }
 
+        static int CheckValidInput(int min, int max)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
 
+                bool parseSuccess = int.TryParse(input, out var ret);
+                if (parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
+
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+        }
+
+        public void WinBoard(List<Monster> monster, Player player)
+        {
+            int rewardGold;
+            int rewardExp;
+            rewardGold = monster[0].Gold + monster[1].Gold + monster[2].Gold;
+            rewardExp = monster[0].RewardExp + monster[1].RewardExp + monster[2].RewardExp;
+            player.Gold += rewardGold;
+            player.LevelUpExp += rewardExp;
+            // stage++;
+        }
     }
 }
--- View/BattleView.cs
+++ View/BattleView.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using text_rpg.Characters;
 using text_rpg.Controller;
-using text_rpg.dungeon;
 
 namespace text_rpg.View
 {
@@ -18,6 +17,7 @@ namespace text_rpg.View
 
     internal class BattleView
     {
+        
         public void View(List<Monster> monsters)
         {
             
@@ -37,63 +37,14 @@ namespace text_rpg.View
             Console.WriteLine("= 0. 나가기      =");
             Console.WriteLine("==================");
         }
-        private void dungeonEnter()
-        {
-            //종욱님 그림
-            DungeonSelectView();
+        
 
-            int firstView = (int)Select.EnterDungeon;
-            firstView = CheckValidInput(0, 2);
-            Select enumValue = (Select)firstView;
-
-            switch (enumValue)
-            {
-                case Select.EnterDungeon:
-                    // phase = d_phase.battle
-                    // 던전입장
-                    break;
-
-                case Select.UseItem:
-
-                    break;
-
-                case Select.exit:
-                    // 메인으로 나가기
-                    break;
-
-            }
-        }
-        public void WinBoard(List<Monster> monster, Player player)
-        {
-            int rewardGold;
-            int rewardExp;
-            rewardGold = monster[0].Gold + monster[1].Gold + monster[2].Gold;
-            rewardExp = monster[0].RewardExp + monster[1].RewardExp + monster[2].RewardExp;
-            player.Gold += rewardGold;
-            player.LevelUpExp += rewardExp;
-            // stage++;
-        }
 
         public void LoseBoard()
         {
             Console.WriteLine("키 입력으로  던전 입구로 돌아가기");
         }
 
-        static int CheckValidInput(int min, int max)
-        {
-            while (true)
-            {
-                string input = Console.ReadLine();
-
-                bool parseSuccess = int.TryParse(input, out var ret);
-                if (parseSuccess)
-                {
-                    if (ret >= min && ret <= max)
-                        return ret;
-                }
-
-                Console.WriteLine("잘못된 입력입니다.");
-            }
-        }
+        
     }
 }
